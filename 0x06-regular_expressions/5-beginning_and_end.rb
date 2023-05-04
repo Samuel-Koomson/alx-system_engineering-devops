a93934bdd5ba058c7053c4adada6bb5a8681a216
Samuel <kskoomson1@gmail.com>
2023-05-03 18:10:07 -0700

    Bash, regular expression project done

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# Regular expression which  matches a string with specific  starting and ending and has single characters in-between
+puts ARGV[0].scan(/h.n/).join
---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# Regular expression which matches capital letters only
+puts ARGV[0].scan(/[A-Z]/).join
